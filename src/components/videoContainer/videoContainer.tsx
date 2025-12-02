08d3cf3a3d69e5fa5a630bacdc6251dc02dc4ed5
Maksym Zinchenko <awesome20051969@gmail.com>
2025-12-02 18:49:37 +0100

    Previous-Next-Buttons logic is added

--- src/components/videoContainer/videoContainer.tsx
+++ src/components/videoContainer/videoContainer.tsx
@@ -1,23 +1,38 @@
 import type { VideoPageContent } from "src/pages/videoPages/videoPageText";
 import styles from "./videoContainer.module.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface props {
     content: VideoPageContent[];
 }
 
 export function VideoContent({content}: props){
-
+    const navigate = useNavigate();
     const {id} = useParams<{id: string}>();
 
     const lessonVideo = content.find((cLV) => cLV.id === Number(id));
 
-    return(
+    return (
         <>    
             <div className={styles.initialContainer}>
                 <h1 className={styles.pageTitle}>{lessonVideo?.videoTitle}</h1>
                 <em className={styles.pageP}>{lessonVideo?.videoShortDescription}</em>
+                <button 
+                    onClick = {() => 
+                        Number(id) > 1?
+                        navigate(`/lessons/videos/${Number(id) - 1}`)
+                        : navigate('/lessons/videos/7')}>Previous
+                </button>
+                <button 
+                    onClick = {() => 
+                        Number(id) < 7?
+                        navigate(`/lessons/videos/${Number(id) + 1}`)
+                        : navigate('/lessons/videos/1')}>Next
+                </button>
             </div>
         </>
     )
 }
+
+// Number(id) > 1? navigate(`/lessons/videos/${Number(id) - 1}: navigate('/lessons/videos/7')
+// onClick = {() => navigate(`/lessons/videos/${Number(id) - 1}`)}
