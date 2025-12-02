6c26c48fd008c91588b19b573cba68fcd5c50364
Maksym Zinchenko <awesome20051969@gmail.com>
2025-12-02 19:19:11 +0100

    Previous-Next logic update

--- src/components/videoContainer/videoContainer.tsx
+++ src/components/videoContainer/videoContainer.tsx
@@ -10,25 +10,27 @@ export function VideoContent({content}: props){
     const navigate = useNavigate();
     const {id} = useParams<{id: string}>();
 
-    const lessonVideo = content.find((cLV) => cLV.id === Number(id));
+    const lessonVideo = content.find((lv) => lv.id === Number(id));
+
+    const currentId: number = Number(id);
+
+    const goPrev = () => {
+        const prevId = currentId > 1? currentId - 1: content[content.length - 1].id;
+        navigate(`/lessons/videos/${prevId}`);                
+    }
+
+    const goForth = () => {
+        const nextId = currentId < content.length? currentId + 1: content[0].id;
+        navigate(`/lessons/videos/${nextId}`);    
+    }
 
     return (
         <>    
             <div className={styles.initialContainer}>
                 <h1 className={styles.pageTitle}>{lessonVideo?.videoTitle}</h1>
                 <em className={styles.pageP}>{lessonVideo?.videoShortDescription}</em>
-                <button 
-                    onClick = {() => 
-                        Number(id) > 1?
-                        navigate(`/lessons/videos/${Number(id) - 1}`)
-                        : navigate('/lessons/videos/7')}>Previous
-                </button>
-                <button 
-                    onClick = {() => 
-                        Number(id) < 7?
-                        navigate(`/lessons/videos/${Number(id) + 1}`)
-                        : navigate('/lessons/videos/1')}>Next
-                </button>
+                <button onClick = {goPrev}>Previous</button>
+                <button onClick = {goForth}>Next</button>
             </div>
         </>
     )
