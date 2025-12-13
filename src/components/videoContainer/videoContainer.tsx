a1d47a6617c214db75a8a2108732d1003cd36226
Maksym Zinchenko <awesome20051969@gmail.com>
2025-12-13 20:13:08 +0100

    Video Page is ready

--- src/components/videoContainer/videoContainer.tsx
+++ src/components/videoContainer/videoContainer.tsx
@@ -1,6 +1,11 @@
 import type { VideoPageContent } from "src/pages/videoPages/videoPageText";
 import styles from "./videoContainer.module.css";
 import { useNavigate, useParams } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import { AdjustablePillButton } from "../adjustablePillButton/adjustablePillButton";
+import book_icon from 'src/assets/icons/Book_open.png';
+import quiz_icon from 'src/assets/icons/quiz.png';
 
 interface props {
     content: VideoPageContent[];
@@ -30,8 +35,42 @@ export function VideoContent({content}: props){
                 <h1 className={styles.pageTitle}>{lessonVideo?.videoTitle}</h1>
                 <em className={styles.pageP}>{lessonVideo?.videoShortDescription}</em>
                 <div className={styles.btnContainer}>
-                    <button className={styles.prevBtn} id={styles.btn} onClick = {goPrev}>Previous</button>
-                    <button className={styles.nextBtn} id={styles.btn} onClick = {goForth}>Next</button>
+                    <button className={styles.prevBtn} id={styles.btn} onClick = {goPrev}>
+                        <FaArrowLeft className={styles.btnArrow}/>
+                        Previous
+                    </button>
+                    <button className={styles.nextBtn} id={styles.btn} onClick = {goForth}>
+                        Next 
+                        <FaArrowRight className={styles.btnArrow}/>
+                    </button>
+                </div>
+                <iframe src = {lessonVideo?.videoLink} 
+                        referrerPolicy="strict-origin-when-cross-origin" 
+                        allowFullScreen 
+                        className={styles.videoPlaceholder}/>
+                <div className={styles.pageDescription}>
+                    <h2>Video Description</h2>
+                    <p>{lessonVideo?.videoDescription}</p>
+                </div>
+                <div className={styles.btnWrapper}>
+                    <div className={styles.optionBtns}>
+                        <div className={styles.testBtn}>
+                            <AdjustablePillButton 
+                                text={String(lessonVideo?.testYourKnowledgeBtn)} 
+                                darkmode={true} 
+                                icon={quiz_icon}
+                                //onClick={() => navigate(`/quiz/}`)}
+                            />
+                        </div>
+                        <div className={styles.backToBtn}>
+                            <AdjustablePillButton 
+                                text={String(lessonVideo?.backToLessonsBtn)} 
+                                darkmode={true} 
+                                icon={book_icon}
+                                onClick={() => navigate(`/lessons/`)}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/videoPages/videoPageText.tsx
+++ src/pages/videoPages/videoPageText.tsx
@@ -3,7 +3,10 @@ export interface VideoPageContent{
     videoTitle: string,
     videoShortDescription: string,
     videoDescription: string,
-    videoLink: string
+    videoLink: string,
+    testYourKnowledgeBtn: string,
+    backToLessonsBtn: string,
+
 }
 
 export const videoPageContent: VideoPageContent[] = [
@@ -12,47 +15,61 @@ export const videoPageContent: VideoPageContent[] = [
         videoTitle: "Lesson 1 - Video",
         videoShortDescription: "Watch a short Video for more understanding",
         videoDescription: "Reallife example and overview of all the mahjong tiles!",
-        videoLink: "https://youtu.be/7pnzR6kD2Q4?si=6Tu9dLNDemyNESvL"
+        videoLink: "https://www.youtube.com/embed/7pnzR6kD2Q4?si=-TMxdPao-Wz190Q5",
+        testYourKnowledgeBtn: "Test your Knowledge",
+        backToLessonsBtn: "Back to lessons"
     },
     {
         id: 2,
         videoTitle: "Lesson 2 - Video",
         videoShortDescription: "Watch a short Video for more understanding",
         videoDescription: "Reallife example and overview of all the mahjong tiles!",
-        videoLink: "https://youtu.be/7pnzR6kD2Q4?si=6Tu9dLNDemyNESvL"
+        videoLink: "https://www.youtube.com/embed/2a-AK_39xg8?si=BC4qfnPD_4FWl-mh",
+        testYourKnowledgeBtn: "Test your Knowledge",
+        backToLessonsBtn: "Back to lessons"
     },
     {
         id: 3,
         videoTitle: "Lesson 3 - Video",
         videoShortDescription: "Watch a short Video for more understanding",
         videoDescription: "Reallife example and overview of all the mahjong tiles!",
-        videoLink: "https://youtu.be/7pnzR6kD2Q4?si=6Tu9dLNDemyNESvL"
+        videoLink: "https://www.youtube.com/embed/7pnzR6kD2Q4?si=-TMxdPao-Wz190Q5",
+        testYourKnowledgeBtn: "Test your Knowledge",
+        backToLessonsBtn: "Back to lessons"
     },
     {
         id: 4,
         videoTitle: "Lesson 4 - Video",
         videoShortDescription: "Watch a short Video for more understanding",
         videoDescription: "Reallife example and overview of all the mahjong tiles!",
-        videoLink: "https://youtu.be/7pnzR6kD2Q4?si=6Tu9dLNDemyNESvL"
+        videoLink: "https://www.youtube.com/embed/7pnzR6kD2Q4?si=-TMxdPao-Wz190Q5",
+        testYourKnowledgeBtn: "Test your Knowledge",
+        backToLessonsBtn: "Back to lessons"
     },
     {   id: 5,
         videoTitle: "Lesson 5 - Video",
         videoShortDescription: "Watch a short Video for more understanding",
         videoDescription: "Reallife example and overview of all the mahjong tiles!",
-        videoLink: "https://youtu.be/7pnzR6kD2Q4?si=6Tu9dLNDemyNESvL"
+        videoLink: "https://www.youtube.com/embed/7pnzR6kD2Q4?si=-TMxdPao-Wz190Q5",
+        testYourKnowledgeBtn: "Test your Knowledge",
+        backToLessonsBtn: "Back to lessons"
     },
     {
         id: 6,
         videoTitle: "Lesson 6 - Video",
         videoShortDescription: "Watch a short Video for more understanding",
         videoDescription: "Reallife example and overview of all the mahjong tiles!",
-        videoLink: "https://youtu.be/7pnzR6kD2Q4?si=6Tu9dLNDemyNESvL"
+        videoLink: "https://www.youtube.com/embed/7pnzR6kD2Q4?si=-TMxdPao-Wz190Q5",
+        testYourKnowledgeBtn: "Test your Knowledge",
+        backToLessonsBtn: "Back to lessons"
     },
     {
         id: 7,
         videoTitle: "Lesson 7 - Video",
         videoShortDescription: "Watch a short Video for more understanding",
         videoDescription: "Reallife example and overview of all the mahjong tiles!",
-        videoLink: "https://youtu.be/7pnzR6kD2Q4?si=6Tu9dLNDemyNESvL"
+        videoLink: "https://www.youtube.com/embed/7pnzR6kD2Q4?si=-TMxdPao-Wz190Q5",
+        testYourKnowledgeBtn: "Test your Knowledge",
+        backToLessonsBtn: "Back to lessons"
     }
 ]
